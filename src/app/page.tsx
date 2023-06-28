7afbb80b7e646a46c2c605b31d4dcb4739ae6f89
Khiem Le <theinnocentman1@gmail.com>
2023-06-28 21:12:01 +0700

    Test picture annotation tool

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,21 @@
 'use client'
+import { ReactPictureAnnotation } from 'react-picture-annotation'
+import { IAnnotation } from 'react-picture-annotation/dist/types/src/Annotation'
 
 export default function Home() {
-  return <main className="bg-red-500">Demo Track Anything</main>
+  return (
+    <main className="">
+      <ReactPictureAnnotation
+        image="https://www.khiemle.dev/_next/image?url=%2Fstatic%2Fthumbnails%2F2020%2FHashTable.jpg&w=1200&q=75"
+        onSelect={(id: string | null) => {
+          console.log(id)
+        }}
+        onChange={(annotationData: IAnnotation[]) => {
+          console.log(annotationData)
+        }}
+        width={1024}
+        height={720}
+      />
+    </main>
+  )
 }
